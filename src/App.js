33e13fd73962016aa4031bab7bea3481a1f53860
Marc Cooperstein <mcooperstein@gmail.com>
2018-05-26 16:34:02 -0700

    added state for user signin

--- src/App.js
+++ src/App.js
@@ -48,6 +48,13 @@ class App extends Component {
       cultural: []
     }
   }
+
+  // componentDidMount(){
+  //   fetch('http://localhost:3000/')
+  //   .then(response => response.json())
+  //   .then(console.log)
+  // }
+
   onInputChange = (event) => {
     this.setState({
       input: event.target.value
